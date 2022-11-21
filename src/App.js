47654024e36adb8a0d411983ff3fd802616510ba
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-21 11:26:14 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import hexToRGBA from "./shared/hexaToRGBA";
 
 function App() {
+  const [hexaInput, setHexaInput] = useState("#156538");
+  const [errorClass, setErrorClass] = useState("null");
+  const [rgbColor, setRGBAColor] = useState("rgb(21, 101, 56)");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const value = e.target.value;
+    setHexaInput(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const firstLetter = hexaInput.charAt(0);
+    if (
+      firstLetter === "#" &&
+      (hexaInput.length === 4 || hexaInput.length === 7)
+    ) {
+      setErrorClass("null");
+      hexToRGBA(hexaInput);
+      // more code for correct input
+    } else {
+      setErrorClass("error");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className={errorClass}
+            placeholder="#f15025"
+            value={hexaInput}
+            onChange={handleChange}
+          />
+          <button className="btn">submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <article className="color" style={{ background: `${rgbColor}` }}>
+          <p className="percent-value">20%</p>
+          <p className="color-value">#526879</p>
+        </article>
+      </section>
+    </>
   );
 }
 
---
+++ src/shared/hexaToRGBA.js
@@ -0,0 +1,5 @@
+function hexToRGBA(d) {
+  return (+d).toString(16).toUpperCase();
+}
+
+export default hexToRGBA;
