be25d16e9084f9d3030849d3af58a157e5bea6dc
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-22 10:11:43 +0100

    initial commit

--- src/components/SingleColor.js
+++ src/components/SingleColor.js
@@ -1,12 +1,17 @@
 import React from "react";
+import rgbToHex from "../shared/rgbToHexa";
 
 export const SingleColor = ({ rgb, weight, index }) => {
   let rgbColor = rgb.join(",");
+  let hex = rgbToHex(rgb[0], rgb[1], rgb[2]);
 
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${rgbColor})` }}
-    ></article>
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hex}</p>
+    </article>
   );
 };
